36640d9c8289161d3ef405ac4fb46fb86d6c2f46
Guifrema <115832046+Guifrema@users.noreply.github.com>
2024-11-13 13:52:24 -0300

    Design do Login e Cadastro atualizados

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/cadastroUsuario/cadastrar-usuario/cadastrar-usuario.component.html",
+    "./src/app/login/login/login.component.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

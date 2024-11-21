4904dfcb5666eb70a26e7308dd4f169dc68ac23b
Guifrema <115832046+Guifrema@users.noreply.github.com>
2024-11-21 18:01:47 -0300

    Atualizado o serviço responsável pelas avaliações

--- src/app/lista-avaliacoes/lista-avaliacoes.component.ts
+++ src/app/lista-avaliacoes/lista-avaliacoes.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { AvalicaoInterface, AvalicaoResponse } from '../../interfaces/AvaliacaoInterface';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+import { AvaliacoesService } from '../services/avalicao.service';
 
 
 @Component({
@@ -10,10 +18,19 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrl: './lista-avaliacoes.component.css'
 })
 export class ListaAvaliacoesComponent {
+  avaliacoes: AvalicaoResponse[] = [];
+  errorMessage: string = '';
 
   constructor(
+    private avaliacaoService: AvaliacoesService,
     private router: Router,
+    private http: HttpClient,
+    private cdRef: ChangeDetectorRef
   ) {}
+
+  ngOnInit(): void {
+
+  }
 
   navigateToFestas(): void {
     this.router.navigate(['/lobby']);
